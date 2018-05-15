fe4b0784748b26a7822726076dd7a29bf877c610
ngoc.cao <ngoc.cm2993@gmail.com>
2018-05-15 23:58:42 +0700

    upload multi file

--- Coding/voice_dashboard/pdf_parse/urls.py
+++ Coding/voice_dashboard/pdf_parse/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^v1', views.upload_file, name='v1'),
-    url(r'^pdf_parse',views.parse_pdf, name='parse_pdf')
+    url(r'^form/$', views.upload_form),
+    url(r'^upload/$', views.upload_multi_file),
 ]
--- Coding/voice_dashboard/pdf_parse/views.py
+++ Coding/voice_dashboard/pdf_parse/views.py
@@ -7,40 +7,21 @@ import PyPDF2 as pyPdf
 from django.http import JsonResponse, HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
-from .forms import UploadFileForm
-from .models import Document
+from voice_dashboard import settings
 # Create your views here.
 from django.core.files.storage import FileSystemStorage
 
-def upload_file(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            newdoc = Document(docfile=request.FILES['docfile'])
-            newdoc.save()
-            handle_uploaded_file(request.FILES['docfile'])
-            return HttpResponseRedirect(reverse('list'))
-    else:
-        form = UploadFileForm()
-    # Load documents for the list page
-    documents = Document.objects.all()
 
-    # Render list page with the documents and the form
-    return render(
-        request,
-        'list.html',
-        {'documents': documents, 'form': form}
-    )
-    # if request.method == 'POST' and request.FILES['myfile']:
-    #     myfile = request.FILES['myfile']
-    #     fs = FileSystemStorage()
-    #     filename = fs.save(myfile.name, myfile)
-    #     uploaded_file_url = fs.url(filename)
-    #     return render(request, 'upload.html', {
-    #         'uploaded_file_url': uploaded_file_url
-    #     })
-    # return render(request, 'upload.html')
+def upload_form(request):
+    return render(request, "upload.html", {})
+
+
+def upload_multi_file(request):
+    if request.method == 'POST':
+        files = request.FILES.getlist("files")
+        for file in files:
+            handle_uploaded_file(file)
+    return HttpResponse("File(s) uploaded!")
 
 
 def parse_pdf(request):
@@ -49,28 +30,23 @@ def parse_pdf(request):
 
 
 def handle_uploaded_file(f):
-    with open('name.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
+    fs = FileSystemStorage(location='document/')
+    fs.save(f.name, f)
+    split_page(f)
 
 
-def split_page(pdf_file):
-    print (pdf_file)
-    content = pyPdf.PdfFileReader(pdf_file)
-    # filePath = os.path.join(DATA_PATH, fileName.split('.')[0])
-    # if not os.path.exists(filePath):
-    #     print ('vao 1')
-    #     try:
-    #         os.makedirs(filePath)
-    #     except OSError as exc:  # Guard against race condition
-    #         if exc.errno != errno.EEXIST:
-    #             raise
+def split_page(file):
+    content = pyPdf.PdfFileReader(file)
+    sub_dir = file.name.split(' ')[0]
+    file_path = os.path.join(settings.BASE_DIR, 'document\\' + sub_dir)
+    if not os.path.exists(file_path):
+        try:
+            os.makedirs(file_path)
+        except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                raise
     for page in range (content.getNumPages()):
-        # content = pdfContent.getPage(page)
-        # filePath = os.path.join(DATA_PATH, fileName, 'page', str(page))
-        outputFile = pyPdf.PdfFileWriter()
-        outputFile.addPage(content.getPage(page))
-        # with open(os.path.join(filePath, 'page_' + str(page) + '.pdf'), "wb") as outputStream:
-        with open('page_' + str(page) + '.pdf', "wb") as outputStream:
-            outputFile.write(outputStream)
-    # convert_to_csv(filePath)
+        output_file = pyPdf.PdfFileWriter()
+        output_file.addPage(content.getPage(page))
+        with open(os.path.join(file_path, 'page_' + str(page) + '.pdf'), "wb+") as outputStream:
+            output_file.write(outputStream)
